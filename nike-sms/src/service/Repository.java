0ac971bc27e2d974321533e2e46275b8287d165a
Myeongkook Park <myeongkook@vivans.net>
2022-03-11 16:43:23 +0900

    3/11 jar 추가

--- nike-sms/src/service/Repository.java
+++ nike-sms/src/service/Repository.java
@@ -3,9 +3,9 @@ package service;
 import java.sql.*;
 
 public class Repository {
-    static String URL = "";
-    static String USER = "";
-    static String PW = "";
+    static String URL = "jdbc:mysql://15.164.131.180:3306/nike";
+    static String USER = "manager";
+    static String PW = "qweqwe1";
 
     public static void main(String[] args) throws SQLException {
         ResultSet rs;
@@ -17,8 +17,9 @@ public class Repository {
             String one = rs.getString(1);
             String two = rs.getString(2);
             String three = rs.getString(3);
+            String four = rs.getString(4);
 
-            System.out.println(one + " " + two + " " + three);
+            System.out.println(one + " " + two + " " + three + " " + four);
         }
     }
 }
