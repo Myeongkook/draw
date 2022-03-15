862487b90e5cef2c3da586078cfecdf948348a48
myeongkook <myeongkuk@hotmail.com>
2022-03-15 13:06:03 +0900

    03/15 App 컴포넌트 작업

--- nike-frontend/src/App.js
+++ nike-frontend/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <div className="logo">Untitled App</div>
+    </div>
+  );
 }
 
 export default App;
