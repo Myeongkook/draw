c50b6aaed7c9b07ca6bdf7fad55c4e6b103f95a8
myeongkook <myeongkuk@hotmail.com>
2022-03-26 16:48:05 +0900

    3/26 Menu 컴포넌트 css 수정

--- nike-frontend/src/components/Menu.js
+++ nike-frontend/src/components/Menu.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { GiCancel } from 'react-icons/gi';
+import { Link } from 'react-router-dom';
 
 const Menu = () => {
   const unActiveMenu = (e) => {
@@ -7,10 +9,21 @@ const Menu = () => {
   };
   return (
     <div className="menu">
-      <button type="button" onClick={unActiveMenu}>
-        X
-      </button>
-      MENU
+      <div className="header">
+        <GiCancel onClick={unActiveMenu} />
+        MENU
+      </div>
+      <div className="menu-items">
+        <ul>
+          <Link to="/mypage">
+            <li>My Page</li>
+          </Link>
+          <li>Re-Sell</li>
+          <li>Draw</li>
+          <li>Logout</li>
+        </ul>
+      </div>
+      <div className="footer">Untitled App</div>
     </div>
   );
 };
