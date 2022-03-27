7a9b80e6617054a4197dfab7467d08ad7cfac018
myeongkook <myeongkuk@hotmail.com>
2022-03-27 18:27:45 +0900

    3/27 mypage 컴포넌트 css 수정

--- nike-frontend/src/components/mypage.js
+++ nike-frontend/src/components/mypage.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import Layout from './Layout';
+import { GrKey } from 'react-icons/gr';
+import { BiShoppingBag, BiMessageCheck, BiLogOut } from 'react-icons/bi';
 
 const Mypage = () => {
   return (
     <Layout>
       <div className="content">
-        <div className="mypage"> Mypage</div>
+        <div className="mypage">
+          <div className="header">MY PAGE</div>
+          <div className="content">
+            <ul>
+              <li>
+                <BiShoppingBag />
+                MY DEAL
+              </li>
+              <li>
+                <GrKey />
+                PASSWORD CHANGE
+              </li>
+              <li>
+                <BiMessageCheck />
+                SUBSCRIPTION
+              </li>
+              <li>
+                <BiLogOut />
+                LOGOUT
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </Layout>
   );
