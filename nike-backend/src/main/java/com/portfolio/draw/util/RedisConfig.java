3c6783de5da8a48fd5a1bc57be649f8b93d1ce03
myeongkook <myeongkuk@hotmail.com>
2022-04-10 22:58:44 +0900

    🛠Fix: Redis 서버 EC2로 변경, @Value 어노테이션 사용

--- nike-backend/src/main/java/com/portfolio/draw/util/RedisConfig.java
+++ nike-backend/src/main/java/com/portfolio/draw/util/RedisConfig.java
@@ -1,16 +1,29 @@
 package com.portfolio.draw.util;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
 public class RedisConfig {
 
+    private String host;
+    private int port;
+    private String password;
+
+    public RedisConfig(@Value("${spring.redis.host}") String host, @Value("${spring.redis.port}") int port, @Value("${spring.redis.password}") String password) {
+        this.host = host;
+        this.port = port;
+        this.password = password;
+    }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+        redisStandaloneConfiguration.setPassword(password);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 }
