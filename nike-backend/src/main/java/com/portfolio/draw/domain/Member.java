9878d7e8179ac94e2b38d800dfdc5f7a35a90426
myeongkook <myeongkuk@hotmail.com>
2022-04-18 12:54:54 +0900

    🛠Refactor: Entity 에서 Salt 컬럼 삭제

--- nike-backend/src/main/java/com/portfolio/draw/domain/Member.java
+++ nike-backend/src/main/java/com/portfolio/draw/domain/Member.java
@@ -17,8 +17,8 @@ public class Member {
     private Long memberId;
     private String id;
     private String pw;
+    @Column(unique = true)
     private String name;
-    private String salt;
     private String phone;
 
     @CreationTimestamp
