dd35416f981601970f91af3eaf88ec5fd6650a33
myeongkook <myeongkuk@hotmail.com>
2022-04-30 16:45:09 +0900

    🐛Fix: 스프링시큐리티 접근권한 수정

---
+++ nike-backend/src/main/java/com/portfolio/draw/dto/PhoneCertificationDto.java
@@ -0,0 +1,11 @@
+package com.portfolio.draw.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PhoneCertificationDto {
+    private String PhoneNumber;
+    private String CertificationNumber;
+}
